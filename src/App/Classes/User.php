850b39afb96ba8ac3d050c75426e268cc78db2af
hadi <hadi77.hg@gmail.com>
2021-11-30 12:25:17 +0330

    added User model

---
+++ src/App/Classes/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Classes;
+
+class User 
+{
+    private $table = 'users' ;
+
+    public function select ()
+    {
+        //
+    } 
+    public function insert ()
+    {
+        //
+    } 
+    public function update ()
+    {
+        //
+    } 
+    public function delete ()
+    {
+        //
+    } 
+
+
+
+}
